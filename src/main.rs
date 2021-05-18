4975e3a647afd4c3b43e3635e606f7fee774f6d7
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-05-18 19:35:22 +0600

    use dotenv for set HOST and PORT variables

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -12,12 +14,16 @@ async fn hello_test() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let host = env::var("HOST").unwrap_or("127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or("8080".to_string());
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(hello_test)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("{0}:{1}", host, port))?
     .run()
     .await
 }

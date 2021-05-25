6c2fa912d0dad9de61296a0a13626812e0ef6a7b
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-05-25 20:26:29 +0600

    implement auth logic for api/url_insert

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, post, Responder, HttpResponse, web, HttpRequest};
 use crate::service::InsertUrlDto;
+use std::env;
 
 #[get("/{id}")]
 pub async fn find_url_and_redirect(
@@ -9,29 +10,55 @@ pub async fn find_url_and_redirect(
     let id: String = req.match_info().get("id").unwrap().parse().unwrap();
     let result = app_data.service_container.url.find_by_key(&id).await;
     match result {
-        Ok(document) => {
-            if document.is_none() {
+        Ok(url_data) => {
+            if url_data.is_none() {
+                // TODO: add 404 html page
                 return HttpResponse::NotFound().finish()
             }
-            HttpResponse::Found().header("Location", document.unwrap().url).finish()
-        },
+            HttpResponse::Found().header("Location", url_data.unwrap().url).finish()
+        }
+        // TODO: add 404 html page
         Err(_) => HttpResponse::NotFound().finish()
     }
 }
 
 #[post("/api/insert-url")]
-pub async fn insert_url(insert_url_dto: web::Json<InsertUrlDto>, app_data: web::Data<crate::AppState>) -> impl Responder {
+pub async fn insert_url(req: HttpRequest, insert_url_dto: web::Json<InsertUrlDto>, app_data: web::Data<crate::AppState>) -> impl Responder {
+    if !self::is_auth(&req) {
+        // TODO: add 403 error response
+        return HttpResponse::Forbidden().finish()
+    }
+
     let result = app_data.service_container.url.create(&insert_url_dto.url).await;
     match result {
         Ok(created_url_dto) => {
             if created_url_dto.is_none() {
+                // TODO: add 404 error response
                 return HttpResponse::NotFound().finish()
             }
             HttpResponse::Ok().json(created_url_dto.unwrap())
         },
         Err(_) => {
+            // TODO: add 5xx error response
             HttpResponse::InternalServerError().finish()
         }
     }
+}
 
+fn is_auth(req: &HttpRequest) -> bool {
+    let mut is_auth = false;
+    let token_header = req.headers().get("X-API-TOKEN");
+
+    let token = match token_header {
+        Some(header) => {
+            header.to_str().unwrap().to_string()
+        },
+        None => "".to_string()
+    };
+
+    if token == env::var("API_TOKEN").unwrap() {
+        is_auth = true
+    }
+
+    is_auth
 }
